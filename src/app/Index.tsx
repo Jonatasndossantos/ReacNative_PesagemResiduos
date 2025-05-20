a891090bfbd07273613088eb3c9e0d912d4082bb
JONATAS NASCIMENTO DOS SANTOS <jonatas.nsantos2@senacsp.edu.br>
2025-05-20 17:25:13 -0300

    botao

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,7 +1,8 @@
-import {View, Text, StyleSheet, Button, Alert, ImageBackground} from 'react-native';
+import {View, Text, StyleSheet, Button, Alert, ImageBackground, TouchableOpacity} from 'react-native';
 import {useState} from 'react';
 import {useNavigation} from 'expo-router';
 import Fundo from '../../assets/images/Fundo.jpg';
+import materialIcon from '@expo/vector-icons'
 
 export default function Index(){
 
@@ -10,7 +11,10 @@ export default function Index(){
             <ImageBackground source={Fundo} resizeMode="cover" style={styles.container}>
                 <Text style={styles.text}>Inside</Text>
                 <View style={styles.card}>
-
+                    <TouchableOpacity style={styles.btn}>
+                        <Text>texte</Text>
+                    </TouchableOpacity>
+                    
                 </View>
             </ImageBackground>
         </View>
@@ -29,8 +33,26 @@ const styles = StyleSheet.create({
         width: 350,
         height: 400,
         borderRadius: 25,
+        padding: 20,
         opacity: 0.5,
         backgroundColor: "#FFFFFF",
 
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 3,
+            height: 10,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 10,
+
+        elevation: 10,
     },
+    btn:{
+        width: "100%",
+        height: 50,
+        padding: 10,
+        alignItems: "center",
+        backgroundColor: "#EEF2ED",
+        borderRadius: 10,
+    }
 });
